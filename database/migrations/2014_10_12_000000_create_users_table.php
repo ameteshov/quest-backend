787661829c84df4aeabcac2de7722e2e4c506e82
ameteshov <ameteshov@ronasit.com>
2018-12-24 21:55:31 +0700

    #-: fix migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email', 100)->unique();
             $table->string('password')->nullable();
             $table->string('reset_token')->nullable();
             $table->integer('role_id');
